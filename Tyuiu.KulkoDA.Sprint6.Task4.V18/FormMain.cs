74d697b285c9b40888e939a436bacd2f65342717
Tyuiu.KulkoDA@outlook.com <Tyuiu.KulkoDA@outlook.com>
2024-12-07 22:49:23 +0500

    Task4

---
+++ Tyuiu.KulkoDA.Sprint6.Task4.V18/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.KulkoDA.Sprint6.Task4.V18
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
