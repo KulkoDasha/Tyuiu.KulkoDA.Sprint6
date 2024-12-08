96c8c2f9a6185672599c51a246535dfc380c95c0
Tyuiu.KulkoDA@outlook.com <Tyuiu.KulkoDA@outlook.com>
2024-12-08 17:18:59 +0500

    Task4

--- Tyuiu.KulkoDA.Sprint6.Task4.V18/FormMain.Designer.cs
+++ Tyuiu.KulkoDA.Sprint6.Task4.V18/FormMain.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             textBoxDone = new TextBox();
             textBoxStart = new TextBox();
@@ -44,17 +44,17 @@
             // chart1
             // 
             chart1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            chartArea2.Name = "ChartArea1";
-            chart1.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            chart1.Legends.Add(legend2);
+            chartArea3.Name = "ChartArea1";
+            chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            chart1.Legends.Add(legend3);
             chart1.Location = new Point(231, 90);
             chart1.Name = "chart1";
-            series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            chart1.Series.Add(series2);
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            chart1.Series.Add(series3);
             chart1.Size = new Size(612, 395);
             chart1.TabIndex = 0;
             chart1.Text = "chart1";
@@ -83,31 +83,34 @@
             // 
             // buttonDone
             // 
+            buttonDone.BackColor = Color.FromArgb(192, 192, 255);
             buttonDone.Location = new Point(583, 26);
             buttonDone.Name = "buttonDone";
             buttonDone.Size = new Size(78, 58);
             buttonDone.TabIndex = 4;
             buttonDone.Text = "Выполнить";
-            buttonDone.UseVisualStyleBackColor = true;
+            buttonDone.UseVisualStyleBackColor = false;
             buttonDone.Click += buttonDone_Click;
             // 
             // buttonSave
             // 
+            buttonSave.BackColor = Color.FromArgb(255, 192, 192);
             buttonSave.Location = new Point(667, 26);
             buttonSave.Name = "buttonSave";
             buttonSave.Size = new Size(78, 58);
             buttonSave.TabIndex = 5;
             buttonSave.Text = "Сохранить";
-            buttonSave.UseVisualStyleBackColor = true;
+            buttonSave.UseVisualStyleBackColor = false;
             // 
             // buttonHelp
             // 
+            buttonHelp.BackColor = Color.FromArgb(255, 224, 192);
             buttonHelp.Location = new Point(751, 26);
             buttonHelp.Name = "buttonHelp";
             buttonHelp.Size = new Size(78, 58);
             buttonHelp.TabIndex = 6;
             buttonHelp.Text = "Справка";
-            buttonHelp.UseVisualStyleBackColor = true;
+            buttonHelp.UseVisualStyleBackColor = false;
             // 
             // FormMain
             // 
