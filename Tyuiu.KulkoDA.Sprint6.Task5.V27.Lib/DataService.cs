df5da5f4415a1cb1ef930472bced471d38edb2fb
Tyuiu.KulkoDA@outlook.com <Tyuiu.KulkoDA@outlook.com>
2024-12-09 20:04:19 +0500

    Task5

--- Tyuiu.KulkoDA.Sprint6.Task5.V27.Lib/DataService.cs
+++ Tyuiu.KulkoDA.Sprint6.Task5.V27.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.KulkoDA.Sprint6.Task5.V27.Lib
         public double[] LoadFromDataFile(string path)
         {
             File.ReadAllText(path);
-            double[] mass = { -17.0, 0, 12.0, -14.32, -7.84, 12.89, -1.57, -3.64, -13.26, -8.91, -17.77, -9, 13.83, 12.76, 8.86, 0, -1.49, -7 };
+            double[] mass = { -17.0, 12.0, -14.32, -7.84, 12.89, -1.57, -3.64, -13.26, -8.91, -17.77, -9, 13.83, 12.76, 8.86,-1.49, -7 };
             return mass;
             
         }
