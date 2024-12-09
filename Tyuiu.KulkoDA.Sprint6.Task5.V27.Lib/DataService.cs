1943d32f5a4aa3b48c705359b22848f9e8e62c55
Tyuiu.KulkoDA@outlook.com <Tyuiu.KulkoDA@outlook.com>
2024-12-09 19:34:30 +0500

    Task0

---
+++ Tyuiu.KulkoDA.Sprint6.Task5.V27.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.KulkoDA.Sprint6.Task5.V27.Lib
+{
+    public class DataService : ISprint6Task5V27
+    {
+        public double[] LoadFromDataFile(string path)
+        {
+            double[] a = new double[20]; 
+            double res = 0;
+            using(StreamReader sr = new StreamReader(path))
+            {
+                string line;
+                while((line = sr.ReadLine()) != null)
+                {
+                    for(int i = 0; i < a.Length; i++)
+                    {
+                        a[i] = Convert.ToDouble(line);
+                        
+                    }
+                }
+            }
+            return a;
+        }
+    }
+}
---
+++ Tyuiu.KulkoDA.Sprint6.Task5.V27.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.KulkoDA.Sprint6.Task5.V27.Lib;
+namespace Tyuiu.KulkoDA.Sprint6.Task5.V27.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\InPutFileTask5V27.txt";
+            var res = ds.LoadFromDataFile(path);
+            double[] mass = { -17.0, 0, 12.0, -14.32, 5, -7.84, 12.89, -1.57, -3.64, -13.26, -8.91, -17.77,35, -9, 13.83, 12.76, 8.86, 0, -1.49, -7 };
+            CollectionAssert.AreEqual(mass, res);
+        }
+    }
+}
