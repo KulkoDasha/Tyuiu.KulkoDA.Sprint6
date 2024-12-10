7ae5bc48b22d8e8a2776fe7550be98d557203fc8
Tyuiu.KulkoDA@outlook.com <Tyuiu.KulkoDA@outlook.com>
2024-12-10 19:45:53 +0500

    Task6

--- Tyuiu.KulkoDA.Sprint6.Task6.V8.Lib/DataService.cs
+++ Tyuiu.KulkoDA.Sprint6.Task6.V8.Lib/DataService.cs
@@ -1,42 +1,18 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint6;
+﻿using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint6;
 namespace Tyuiu.KulkoDA.Sprint6.Task6.V8.Lib
 {
     public class DataService : ISprint6Task6V8
     {
-        public int len;
+        
         public string CollectTextFromFile(string path)
         {
-            using (StreamReader reader = new StreamReader(path))
-            {
-                string line;
-                while ((line = reader.ReadLine()) != null)
-                {
-                    len++;
-                }
-            }
-            string resStr = "";
-            string str = "z";
-            string[] a = new string[len];
-            using (StreamReader reader = new StreamReader(path))
-            {
-                string line;
-                while ((line = reader.ReadLine()) != null)
-                {
-                    if (line.Contains(str))
-                    {
-                        a = line.Split(' ').ToArray();
-                    }
-                }
-                for(int i = 0; i < a.Length; i++)
-                {
-                    if(a.Contains(str))
-                    {
-                        resStr = resStr+" " +a[i];
-                    }
-                } 
-                
-            }
-            return resStr ;
+            string[] a = File.ReadAllLines(path);
+            string str = "";
+            StringBuilder sb = new StringBuilder(a.Length);
+            str = sb.Append(a).ToString();
+            
+            return str;
         }
     }
 }
